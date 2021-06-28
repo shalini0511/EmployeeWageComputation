bad224e4bbe07e47de22e319e63a122a12b3d128
shalini <shalini.v@tvsnext.io>
2021-06-28 22:30:09 +0530

    monthly wages

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,28 +7,30 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             //local variable
-
-            int employeeState = 1, partTimeState = 2;
-            int dailyWage, wageperHour = 20, hours = 8, partTimeHour = 4;
+            int dailyWage, wageperHour = 20, hours = 8, partTimeHour = 4, days = 20;
             Random random = new Random();
             int Attendance = random.Next(0, 3);
             Console.WriteLine("Random number is :" + Attendance);
+            switch (Attendance)
+            {
+                case 1:
+                    Console.WriteLine("Employee is Present \n");
+                    break;
+                case 2:
+                    Console.WriteLine("Parttimer is Present");
+                    hours = partTimeHour; Attendance = 1;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent \n");
+                    break;
+            }
 
-            if ((partTimeState == Attendance) || (Attendance == employeeState))
-            {
-                Console.WriteLine("Employee is Present \n");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent \n");
-
-            }
-            if (Attendance == 2)
-            {
-                hours = partTimeHour; Attendance = 1;
-            }
             dailyWage = wageperHour * hours * Attendance;
             Console.WriteLine("Employee daily wage for 20/hr is : " + dailyWage);
+            Console.WriteLine("Employee wage for 20 days  is : " + dailyWage * days);
+
+
+
         }
 
     }
