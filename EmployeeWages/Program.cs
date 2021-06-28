f3d61c5f14f076414320e0acd81a22ece7aa0832
shalini <shalini.v@tvsnext.io>
2021-06-28 23:25:35 +0530

    using while condition

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,28 +7,45 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             //local variable
-            int dailyWage, wageperHour = 20, hours = 8, partTimeHour = 4, days = 20;
+            int employeeStatus;
+            int totalWorkingDays = 0;
+            int totalWorkingHours = 0;
+            int monthlyWage = 0;
+
+
+            int wagePerHour = 20;
             Random random = new Random();
-            int Attendance = random.Next(0, 3);
-            Console.WriteLine("Random number is :" + Attendance);
-            switch (Attendance)
+            employeeStatus = random.Next(0, 3);
+
+            switch (employeeStatus)
             {
                 case 1:
-                    Console.WriteLine("Employee is Present \n");
+                    Console.WriteLine("Full time- Employee Present");
+                    while (totalWorkingDays <= 20 && totalWorkingHours <= 100)
+                    {
+                        totalWorkingHours = totalWorkingHours + 8;
+                        totalWorkingDays++;
+                        monthlyWage = monthlyWage + (wagePerHour * 8);
+                    }
+                    Console.WriteLine("Total wage for month is:" + monthlyWage);
+
+                    break;
+                case 0:
+                    Console.WriteLine("Employee Absent");
                     break;
                 case 2:
-                    Console.WriteLine("Parttimer is Present");
-                    hours = partTimeHour; Attendance = 1;
+                    Console.WriteLine("part time- Employee Present");
+                    while (totalWorkingDays <= 20 && totalWorkingHours <= 100)
+                    {
+                        totalWorkingHours = totalWorkingHours + 4;
+                        totalWorkingDays++;
+                        monthlyWage = monthlyWage + (wagePerHour * 4);
+                    }
+                    Console.WriteLine("Total wage for month is: " + monthlyWage);
                     break;
-                default:
-                    Console.WriteLine("Employee is Absent \n");
-                    break;
+
             }
-
-            dailyWage = wageperHour * hours * Attendance;
-            Console.WriteLine("Employee daily wage for 20/hr is : " + dailyWage);
-            Console.WriteLine("Employee wage for 20 days  is : " + dailyWage * days);
-
+            
 
 
         }
