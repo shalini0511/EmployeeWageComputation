7fa2290aab90db5b5a843e6fc0a59fe222ad9825
shalini <shalini.v@tvsnext.io>
2021-06-28 21:47:44 +0530

    Employee Attendance

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeState = 1;
+            Random random = new Random();
+            int Attendance = random.Next(0, 2);
+            Console.WriteLine("Random number is :" + Attendance);
+
+            if (Attendance == employeeState)
+            {
+                Console.WriteLine("Employee is Present \n");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent \n");
+
+            }
+        }
+    }
+    
+}
