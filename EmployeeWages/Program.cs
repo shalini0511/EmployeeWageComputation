191728c7561bad78bdd2d5d319b65e221449314b
shalini <shalini.v@tvsnext.io>
2021-06-28 22:02:31 +0530

    Employee Wage

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,20 +6,30 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            int employeeState = 1;
+            //local variable
+            int employeePresent  = 1;
+            int empHours = 0;
+            int empRatePerHour = 20;
+            int empWage = 0;
+            //creating object
             Random random = new Random();
+            //calling Next() for generating random values
             int Attendance = random.Next(0, 2);
             Console.WriteLine("Random number is :" + Attendance);
 
-            if (Attendance == employeeState)
+            if (Attendance == employeePresent)
             {
                 Console.WriteLine("Employee is Present \n");
+                empHours = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent \n");
+                empHours = 0;
 
             }
+            empWage = empRatePerHour * empHours;
+            Console.Read();
         }
     }
     
